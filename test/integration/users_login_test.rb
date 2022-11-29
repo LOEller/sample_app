9bb140d23ead1f10532a4c12a11ebe1c6f5fe64c
Luke Eller <luke.eller@arcadia.com>
2022-11-29 15:57:47 -0500

    finish basic login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,16 +2,50 @@ require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
 
-  test "invalid login renders flash then goes away" do
+  def setup
+    @user = users(:luke)
+  end
+
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
 
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email: @user.email, password: "abc123" } }
     assert_template 'sessions/new'
     assert flash[:danger].present?
+    assert_not is_logged_in?
 
     get root_path
     assert flash[:danger].blank?
+  end
+
+  test "layout links change after valid login" do
+    get login_path
+    assert_template 'sessions/new'
+    assert_select "a[href=?]", login_path
+
+    post login_path, params: { session: { email: @user.email, password: "pass123"}}
+
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+
+    assert_select "a[href=?]", login_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
+  test "logout" do
+    post login_path, params: { session: { email: @user.email, password: "pass123"}}
+    assert_redirected_to @user
+    follow_redirect!
+
+    delete logout_path
+    assert_redirected_to root_path
+    follow_redirect!
+
+    assert_not is_logged_in?
+    assert_template "static_pages/home"
   end
 
 end
