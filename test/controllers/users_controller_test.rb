532e7d68579d494a6c4133aaec3312a360088969
Luke Eller <luke.eller@arcadia.com>
2022-12-06 13:56:33 -0500

    finish section 10.2

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,40 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:luke)
+    @another_user = users(:brody)
+  end
+
   test "should get new" do
     get signup_path
     assert_response :success
+  end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { name: @user.name, email: @user.email } }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit when wrong user logged in" do
+    log_in_as @user
+    get edit_user_path @another_user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
+
+  test "should redirect update when wrong user logged in" do
+    log_in_as @user
+    patch user_path(@another_user), params: { user: { name: @user.name, email: @user.email } }
+    assert flash.empty?
+    assert_redirected_to root_url
   end
 end
