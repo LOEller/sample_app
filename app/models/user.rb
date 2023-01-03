56821b1a529a8495512e9a1f129f3c02358f197c
Luke Eller <luke.eller@arcadia.com>
2023-01-03 16:33:14 -0500

    complete chapter 11.1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -9,10 +9,12 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: true
-  before_save { self.email = email.downcase }
+  before_save :downcase_email
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+  before_create :create_activation_digest
 
 
   # creates a new remember token, assigns to self.remember_token
@@ -53,6 +55,18 @@ class User < ApplicationRecord
     def new_token
       SecureRandom.urlsafe_base64
     end
-
   end
+
+  private
+
+    # Creates and assigns the activation token and digest.
+    def create_activation_digest
+      self.activation_token = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
+
+    # Converts email to all lower-case.
+    def downcase_email
+      email.downcase!
+    end
 end
