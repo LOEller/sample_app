942ab794066798d2729701d07450db6946b03643
Luke Eller <luke.eller@arcadia.com>
2023-02-14 15:15:09 -0500

    finish 11.3

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:luke)
     @non_admin = users(:brody)
+    @not_activated = users(:unactivated)
   end
 
   test "index as including pagination and delete links" do
@@ -29,6 +30,20 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     log_in_as(@non_admin)
     get users_path
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test "index displays only activated users" do
+    log_in_as(@admin)
+    get users_path
+
+    first_page_of_users = User.paginate(page: 1)
+    first_page_of_users.each do |user|
+      if user == @admin # this user activated
+        assert_select 'a[href=?]', user_path(user), count: 1
+      elsif user == @not_activated # this user not activated
+        assert_select 'a[href=?]', user_path(user), count: 0
+      end
+    end
   end
 
 end
